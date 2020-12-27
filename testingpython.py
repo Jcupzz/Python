fb5de008136c5fb44b25e782b1125e79d588b71f
Jcupzz <jcupzz255@gmail.com>
2020-12-27 11:42:18 +0530

    Added password count

--- testingpython.py
+++ testingpython.py
@@ -1,2 +1,8 @@
 _username = input("Hi there!\nPlease tell us your name: ")
 print("Hi "+_username)  
+_password = input("Please create a password: ")
+
+if _password.count<8:
+    print("Please Enter a Password with more than 8 characters.")
+else:
+    print("Account created successfully")
