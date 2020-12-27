f46821d81dc782f6c370b12133981124d374eb48
Jcupzz <jcupzz255@gmail.com>
2020-12-27 11:46:15 +0530

    Failed to add password checking less than 8 or not

--- testingpython.py
+++ testingpython.py
@@ -2,7 +2,7 @@ _username = input("Hi there!\nPlease tell us your name: ")
 print("Hi "+_username)  
 _password = input("Please create a password: ")
 
-if _password.count<8:
+if _password=='8':
     print("Please Enter a Password with more than 8 characters.")
 else:
     print("Account created successfully")
