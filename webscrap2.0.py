475288b343de31670d8d3f071ebd6760cc93220f
Jcupzz <jcupzz255@gmail.com>
2021-01-14 16:39:04 +0530

    Initial commit of csv scraping using pandas for web dev evaluation checking

---
+++ webscrap2.0.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+collist = ['Team name','Intuitiveness','Creativity','Responsiveness','Novelilty'] #column names list
+csvlist = ['jacob.csv','cyril.csv'] # csv files list
+
+# Dictionary variables
+Intuitiveness = 0
+Creativity = 0
+Responsiveness = 0
+Novelilty = 0
+
+# initalize Dictionary
+dict = {
+        'Intuitiveness':Intuitiveness,
+        'Creativity':Creativity,
+        'Responsiveness':Responsiveness,
+        'Novelilty':Novelilty
+        }
+
+#initalize DataFrame
+df = pd.DataFrame()
+
+# Read through each file        
+
+for i in range(len(csvlist)):
+    df = pd.read_csv(csvlist[i],usecols=collist,skipinitialspace=True)
+    print(df)
+
+# jf = pd.read_csv('jacob.csv',usecols=collist,skipinitialspace=True,) # set path of your csv
+# cf = pd.read_csv('cyril.csv',usecols=collist,skipinitialspace=True) # set path of your csv
+
+
+
+dict = {}
+print(jf.iloc[0:21])
+print("################################################################################")
+print(cf.iloc[0:21])
+
+
+
